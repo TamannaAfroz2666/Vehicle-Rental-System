830dd3ac68fd5b1baf886bbb6755035deb9cb668
TamannaAfroz2666 <afroztamanna66@gmail.com>
2025-12-07 20:14:57 +0600

    db ready for production

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,22 +1,34 @@
-import { Pool } from "pg";
-import dotenv from "dotenv";
 
+ import dotenv from "dotenv";
+ import { Pool } from "pg";
 dotenv.config();
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
 
-export const connectDb = async (): Promise<void>  => {
+const isProduction = process.env.NODE_ENV === "production";
+
+export const pool = new Pool(
+  isProduction
+    ? {
+        
+        connectionString: process.env.DATABASE_URL as string,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+       
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT) ,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }
+);
+
+export const connectDb = async (): Promise<void> => {
   try {
     await pool.query("SELECT NOW()");
-    console.log(" Database connected successfully!");
+    console.log("Database connected successfully!");
   } catch (error) {
-    console.error(" Database connection failed:", error);
+    console.error("Database connection failed:", error);
     process.exit(1);
   }
 };
