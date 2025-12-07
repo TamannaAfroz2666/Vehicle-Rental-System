11cee4d839b700f8afaa2df6276bc0e49663567a
TamannaAfroz2666 <afroztamanna66@gmail.com>
2025-12-07 13:01:13 +0600

    update for production in db file

--- src/config/db.ts
+++ src/config/db.ts
@@ -3,20 +3,28 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+const isProduction = process.env.NODE_ENV === "production";
 
-export const connectDb = async (): Promise<void>  => {
+export const pool = new Pool(
+  isProduction
+    ? {
+        connectionString: process.env.DATABASE_URL as string,
+        ssl: { rejectUnauthorized: false },
+      }
+    : {
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT || 5432),
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+      }
+);
+
+export const connectDb = async () => {
   try {
     await pool.query("SELECT NOW()");
     console.log(" Database connected successfully!");
   } catch (error) {
-    console.error(" Database connection failed:", error);
-    process.exit(1);
+    console.error("Database connection failed:", error);
   }
 };
